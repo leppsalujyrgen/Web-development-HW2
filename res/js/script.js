25526cf67569942ecac7202398798d6c2ca60fe4
Toomas Tamm <toomastmm@gmail.com>
2019-10-12 21:32:33 +0300

    Courses are added to table with JQuery

--- res/js/script.js
+++ res/js/script.js
@@ -1,6 +1,21 @@
 $(function () {
 
+    function updateCourses(user, tableId) {
+        let courses = user.getCourses();
+        let table = $("#courses");
+        let index = $("#courses tr").length;
+        courses.forEach(course => {
+                table.append(`<tr><td>${index}</td><td>${course.getTitle()}</td><td>${course.getSemester()}</td><td>${course.getGradePoints()}</td></tr>`);
+                index += 1;
+            }
+        )
+    }
+
     let courses = [
+        new Course("Agile software development", 1, 82),
+        new Course("System modeling", 1, 85),
+        new Course("Object-oriented programming", 2, 99),
+        new Course("Estonian language Level A2", 2, 65),
         new Course("Software Engineering", 3, 100),
         new Course("Web Development", 3, 100),
         new Course("Operating systems ", 3, 55),
@@ -25,10 +40,12 @@ $(function () {
     });
 
     $("#add-course-button").click(function () {
-        if($("#add-course").css("display") === "none")
+        if ($("#add-course").css("display") === "none")
             $("#add-course").css({"display": "inline"});
         else
-            $("#add-course").css({"display" : "none"});
+            $("#add-course").css({"display": "none"});
     });
+
+    updateCourses(user, "courses")
 
 });
