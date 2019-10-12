206755f9b1a7cbb34df04884a34367d7b905e712
Toomas Tamm <toomastmm@gmail.com>
2019-10-12 21:35:38 +0300

    Reduced courses in list to 4 to match exercise requirements

--- res/js/script.js
+++ res/js/script.js
@@ -16,10 +16,10 @@ $(function () {
         new Course("System modeling", 1, 85),
         new Course("Object-oriented programming", 2, 99),
         new Course("Estonian language Level A2", 2, 65),
-        new Course("Software Engineering", 3, 100),
-        new Course("Web Development", 3, 100),
-        new Course("Operating systems ", 3, 55),
-        new Course("Algorithms and Data Structures", 3, 100)
+        //new Course("Software Engineering", 3, 100),
+        //new Course("Web Development", 3, 100),
+        //new Course("Operating systems ", 3, 55),
+        //new Course("Algorithms and Data Structures", 3, 100)
     ];
 
     let user = new User("Linus", "Torvalds", "28/12/1969", "Computer Science", courses);
